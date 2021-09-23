99ec496fb9cdfab8a87a56c4b3d1b89e6021a608
skripsss <78685351+skripsss@users.noreply.github.com>
2021-09-23 11:12:43 +0300

    Add files via upload

---
+++ settings/config.js
@@ -0,0 +1,10 @@
+﻿module.exports = {
+	codename: "ALPHA|BOT",//Кодовое имя
+	namebot: "ALPHA|BOT", //Имя бота
+	version: "3.1", //Версия кода
+	admin: "[mb_vlad|Владислав]", //2 руководитель бота
+	coder: "[mb_vlad|Владислав]]", //Для доната
+	zam: "Не найден", //Заместитель
+	site: "В группе", //Сайт, если есть
+	group_url: "[botteamms|ALPHA|BOT]",//12213
+}
